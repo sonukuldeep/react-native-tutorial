daba2506a1235d47c125e06074d6b6d19977cb8a
sonukuldeep <sonukuldeep@outlook.com>
2022-12-03 10:28:42 +0530

    todo app

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import {useState} from 'react'
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("")
+  const [coarseGoals, setCoarseGoals] = useState([])
+  function goalInputHandler(enteredText){
+    setEnteredGoalText(enteredText)
+  }
+
+  function addGoalHandler(){
+    setCoarseGoals(currentVal=>[...currentVal,enteredGoalText])
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello i'm kuldeep</Text>
-      <StatusBar style="auto" />
+      <View style={styles.space}>
+        <TextInput style={styles.text} placeholder="Your course goals!" onChangeText={goalInputHandler}/>
+        <Button title="Add goals" onPress={addGoalHandler}/>
+      </View>
+      <View>
+        <Text style={styles.space}>List of goals...</Text>
+        <View>
+          {coarseGoals.map((item,index)=><Text key={index}>{item}</Text>)}
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,6 +32,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 30,
+  },
+  space: {
+    margin: 16,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  text: {
+    borderColor: 'green',
+    borderWidth: 1,
+    padding: 18,
+    margin: 16,
   },
 });
